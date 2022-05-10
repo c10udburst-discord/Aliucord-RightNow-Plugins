ba73a32f7c059b73493d374f44b7a95bf113fb1f
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-05-10 13:30:34 +0200

    fix ci

--- build.mjs
+++ build.mjs
@@ -1,4 +1,5 @@
 import { spawnSync } from "child_process";
+import { platform } from "process";
 import { existsSync } from "fs";
 import { join } from "path";
 import { argv, cwd, exit } from "process";
@@ -22,10 +23,16 @@ check(!!plugin, `Usage: ${argv.join(" ")} <PLUGIN>`);
 const path = join(plugin, "index.ts");
 check(existsSync(path), `No such file: ${path}`);
 
-spawnSync("node_modules/.bin/rollup", ["-c", "--configPlugin", "typescript", watch && "--watch"].filter(Boolean), {
+console.log(`Building: ${plugin}`)
+
+const proc = spawnSync((platform === "win32") ? ".\\node_modules\\.bin\\rollup.cmd" : "node_modules/.bin/rollup", ["-c", "--configPlugin", "typescript", watch && "--watch"].filter(Boolean), {
     stdio: "inherit",
     cwd: cwd(),
     env: {
         plugin
     }
 });
+
+if (proc.error) {
+    console.error(proc.error)
+}
