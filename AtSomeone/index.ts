9ae3483a3cd620a51d3f1920615a4f7fc27a4f75
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-05-10 09:09:47 +0200

    Update index.ts

--- AtSomeone/index.ts
+++ AtSomeone/index.ts
@@ -7,7 +7,8 @@ export default class AtSomeone extends Plugin {
         const { getChannel } = getByProps("getChannel");
         const { getMemberIds } = getByProps("getMemberIds");
 
-        before(MessageActions, "sendMessage", (_, message, channelId) => {
+        before(MessageActions, "sendMessage", ctx => {
+            const [channelId, message] = ctx.args;
             if (message.content.includes("@someone")) {
                 const channel = getChannel(channelId)
                 const members = getMemberIds(channel.guildId)
