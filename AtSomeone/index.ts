3c3f1e16404b4f6032fe875195af7ab4649ec7f4
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-05-16 10:59:52 +0200

    fix @someone

--- AtSomeone/index.ts
+++ AtSomeone/index.ts
@@ -3,13 +3,13 @@ import { MessageActions, GuildMemberStore } from 'aliucord/metro';
 
 export default class AtSomeone extends Plugin {
     public async start() {
-        
+
         this.commands.registerCommand({
             name: "@someone",
             description: "@someone randomly on the server",
             options: [],
             execute(_, ctx) {
-                const members = GuildMemberStore.getMemberIds(ctx.channel.guildId.toString());
+                const members = GuildMemberStore.getMemberIds(ctx.channel.guild_id);
                 const member = members[Math.floor(Math.random() * members.length)]
                 MessageActions.sendMessage(ctx.channel.id, {content: `<@${member}>`})
             }
--- Token/index.ts
+++ Token/index.ts
@@ -1,6 +1,5 @@
 import { Plugin } from "aliucord/entities";
 import { getByProps } from "aliucord/metro";
-import { ApplicationCommandOptionType } from "aliucord/api";
 
 export default class Token extends Plugin {
     public async start() {
