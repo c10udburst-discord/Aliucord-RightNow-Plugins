cda708a75ec5a83bac60c8f71af8fdd4d2d99fea
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-05-16 10:34:14 +0200

    tweaks

--- AtSomeone/index.ts
+++ AtSomeone/index.ts
@@ -1,16 +1,15 @@
 import { Plugin } from "aliucord/entities";
-import { getByProps, MessageActions } from 'aliucord/metro';
+import { MessageActions, GuildMemberStore } from 'aliucord/metro';
 
 export default class AtSomeone extends Plugin {
     public async start() {
-        const { getMemberIds } = getByProps("getMemberIds");
         
         this.commands.registerCommand({
             name: "@someone",
             description: "@someone randomly on the server",
             options: [],
             execute(_, ctx) {
-                const members = getMemberIds(ctx.channel.guildId);
+                const members = GuildMemberStore.getMemberIds(ctx.channel.guildId.toString());
                 const member = members[Math.floor(Math.random() * members.length)]
                 MessageActions.sendMessage(ctx.channel.id, {content: `<@${member}>`})
             }
--- HiddenChannels/index.ts
+++ HiddenChannels/index.ts
@@ -1,10 +1,25 @@
 import { Plugin } from "aliucord/entities";
-import { getByProps, Constants } from 'aliucord/metro';
-import { before } from "aliucord/utils/patcher";
+import {
+    getByProps,
+    Constants,
+    ChannelStore,
+    ReactNative as RN,
+    FluxDispatcher
+} from 'aliucord/metro';
+import { before, callOriginal } from "aliucord/utils/patcher";
 
 export default class HiddenChannels extends Plugin {
     public async start() {
-        const permissions = getByProps("getChannelPermissions");
+        const permissions = getByProps("getChannelPermissions", "can");
+
+        function isHidden(channel: string | Object | undefined) {
+            if (channel == undefined) return false;
+            if (typeof channel === 'string') {
+                return !callOriginal(permissions.can, permissions, Constants.Permissions.VIEW_CHANNEL, ChannelStore.getChannel(channel))
+            } else {
+                return !callOriginal(permissions.can, permissions, Constants.Permissions.VIEW_CHANNEL, channel)
+            }
+        }
 
         before(permissions, "can", ctx => {
             const [permId, channel] = ctx.args;
@@ -12,6 +27,54 @@ export default class HiddenChannels extends Plugin {
             if (permId === Constants.Permissions.VIEW_CHANNEL) {
                 ctx.result = true
             }
+        })
+
+        const notifications = getByProps("hasUnread", "hasUnreadPins", "getMentionCount")
+        before(notifications, "hasUnread", ctx => {
+            const [channel] = ctx.args
+            if (isHidden(channel))
+                ctx.result = false;
+        })
+        before(notifications, "hasUnreadPins", ctx => {
+            const [channel] = ctx.args
+            if (isHidden(channel))
+                ctx.result = false;
+        })
+        before(notifications, "getMentionCount", ctx => {
+            const [channel] = ctx.args
+            if (isHidden(channel))
+                ctx.result = 0;
+        })
+
+        const messageFetch = getByProps("fetchMessages")
+        before(messageFetch, "fetchMessages", ctx => {
+            const [_, channel] = ctx.args
+            if (isHidden(channel))
+                ctx.result = null;
+        })
+
+        const navigator = getByProps("selectChannel")
+        before(navigator, "selectChannel", ctx => {
+            const [_, channelId] = ctx.args
+            if (isHidden(channelId)) {
+                FluxDispatcher.dispatch({
+                    type: Constants.ActionTypes.LOAD_MESSAGES_FAILURE,
+                    channelId
+                })
+                const channel = ChannelStore.getChannel(channelId)
+                RN.Alert.alert(
+                    channel.name,
+                    `${channel.topic}`
+                )
+                ctx.result = null;
+            }
+        })
+
+        const Router = getByProps("transitionToGuild")
+        before(Router, "transitionToGuild", ctx => {
+            const [_, channel] = ctx.args
+            if (isHidden(channel))
+                ctx.result = null;
         })
     }
 }
