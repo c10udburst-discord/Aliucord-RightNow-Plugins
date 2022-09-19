66d4bfd297a1d55587cc78254d7ce5a7328601dc
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-09-19 13:56:01 +0200

    update experiments

---
+++ Experiments/index.ts
@@ -0,0 +1,24 @@
+import { Plugin } from 'aliucord/entities';
+import { getByProps, UserStore } from 'aliucord/metro';
+
+export default class Experiments extends Plugin {
+  public async start() {
+    const User = getByProps('isDeveloper');
+    const STAFF = al.metro.getByProps("UserFlags").STAFF;
+    const Nodes = Object.values(User._dispatcher._actionHandlers._dependencyGraph.nodes);
+
+    AliuHermes.unfreeze(User)
+
+    Object.defineProperty(User, 'isDeveloper', {
+      get: () => true,
+      configurable: false,
+    });
+
+    UserStore.getCurrentUser().flags |= STAFF;
+
+    Nodes.find(n => n.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({
+        user: { flags: STAFF }, type: "CONNECTION_OPEN"
+    });
+    Nodes.find(n => n.name == "DeveloperExperimentStore").actionHandler["OVERLAY_INITIALIZE"]();
+  }
+}
