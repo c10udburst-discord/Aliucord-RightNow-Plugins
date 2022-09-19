69b47c8bc7e19484002bc157249716a9f4b7b327
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-09-19 16:02:11 +0200

    Update index.ts

--- Experiments/index.ts
+++ Experiments/index.ts
@@ -1,24 +1,28 @@
 import { Plugin } from 'aliucord/entities';
 import { getByProps, UserStore } from 'aliucord/metro';
+import { before } from "aliucord/utils/patcher";
 
 export default class Experiments extends Plugin {
   public async start() {
-    const User = getByProps('isDeveloper');
-    const STAFF = al.metro.getByProps("UserFlags").STAFF;
-    const Nodes = Object.values(User._dispatcher._actionHandlers._dependencyGraph.nodes);
+    
+    before(UserStore, 'getCurrentUser', ctx => {
+      const User = getByProps('isDeveloper');
+      const STAFF = al.metro.getByProps("UserFlags").STAFF;
+      const Nodes = Object.values(User._dispatcher._actionHandlers._dependencyGraph.nodes);
 
-    AliuHermes.unfreeze(User)
+      AliuHermes.unfreeze(User)
 
-    Object.defineProperty(User, 'isDeveloper', {
-      get: () => true,
-      configurable: false,
+      Object.defineProperty(User, 'isDeveloper', {
+        get: () => true,
+        configurable: false,
+      });
+
+      UserStore.getCurrentUser().flags |= STAFF;
+
+      Nodes.find(n => n.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({
+          user: { flags: STAFF }, type: "CONNECTION_OPEN"
+      });
+      Nodes.find(n => n.name == "DeveloperExperimentStore").actionHandler["OVERLAY_INITIALIZE"]();
     });
-
-    UserStore.getCurrentUser().flags |= STAFF;
-
-    Nodes.find(n => n.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({
-        user: { flags: STAFF }, type: "CONNECTION_OPEN"
-    });
-    Nodes.find(n => n.name == "DeveloperExperimentStore").actionHandler["OVERLAY_INITIALIZE"]();
   }
 }
