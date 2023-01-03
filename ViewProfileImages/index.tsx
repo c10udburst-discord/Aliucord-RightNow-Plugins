6e7a6e0a0c3eecf7305c529d0522d5ff6ef9a147
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-01-03 20:12:53 +0100

    fix viewprofileimages

--- ViewProfileImages/index.tsx
+++ ViewProfileImages/index.tsx
@@ -1,21 +1,37 @@
 import { Plugin } from "aliucord/entities";
-import { getByName, getModule, React, URLOpener } from "aliucord/metro";
+import { getByName, React, URLOpener } from "aliucord/metro";
 import { after } from "aliucord/utils/patcher";
 import { Pressable } from "react-native";
 
 export default class ViewProfileImages extends Plugin {
     public async start() {
         const HeaderAvatar = getByName("HeaderAvatar");
-        
         after(HeaderAvatar, "default", (ctx, component) => {
-            const uri = component?.props?.source?.uri;
+            const [{user}] = ctx.args;
+            const image = user?.getAvatarURL?.(false, 4096, true);
+            if (!image) return;
 
-            const image = uri && uri.replace(/size=\d+/g, "size=4096").replace(".webp", ".png")
-            if (image) {
-                ctx.result = <Pressable onPress={() => URLOpener.openURL(image)}>
-                    {component}
-                </Pressable>
-            }
+            const discrim = user.discriminator % 5;
+            const url = typeof image === 'number' ? `https://cdn.discordapp.com/embed/avatars/${discrim}.png` : image?.replace('.webp', '.png');
+
+            ctx.result = <Pressable onPress={() => URLOpener.openURL(url)}>
+                {component}
+            </Pressable>
+        })
+
+        const ProfileBanner = getByName("ProfileBanner");
+        after(ProfileBanner, "default", (ctx, component) => {
+            const [{bannerSource}] = ctx.args;
+
+            if (typeof bannerSource?.uri !== 'string' || !component) return;
+
+            const url = bannerSource.uri
+                .replace(/(?:\?size=\d{3,4})?$/, '?size=4096')
+                .replace('.webp', '.png');
+
+            ctx.result = <Pressable onPress={() => URLOpener.openURL(url)}>
+                {component}
+            </Pressable>
         })
     }
 }
