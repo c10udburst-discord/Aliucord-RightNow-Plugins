faa5c1127346d46b53037cabe6242dd127be098d
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-01-06 12:54:31 +0100

    add message logger

---
+++ MessageLogger/index.tsx
@@ -0,0 +1,46 @@
+import { Plugin } from "aliucord/entities";
+import { before } from "aliucord/utils/patcher";
+import { FluxDispatcher, MessageStore, ChannelStore, Constants } from "aliucord/metro";
+
+export default class MessageLogger extends Plugin {
+    public async start() {
+        const skipLog = (msg) => {
+            if (!msg) return true;
+            if (msg.flags & Constants.MessageFlags.EPHEMERAL) return true;
+
+            return false;
+        }
+    
+
+        before(FluxDispatcher, "dispatch", ctx => {
+            const [args] = ctx.args;
+
+            if (args.type === "MESSAGE_DELETE") {
+                const msg = MessageStore.getMessage(args.channelId, args.id);
+
+                const wasEdited = msg?.editedTimestamp && !msg.editedTimestamp._isValid;
+                
+                if (!wasEdited && skipLog(msg)) return;
+
+                const content = wasEdited ? msg.content : msg?.content + " `[deleted]`";
+
+                ctx.args[0].type = "MESSAGE_UPDATE";
+                ctx.args[0].message = {
+                    ...msg,
+                    edited_timestamp: "invalid_timestamp",
+                    content,
+                    guild_id: ChannelStore.getChannel(args.channelId)?.guild_id,
+                    flags: msg.flags | Constants.MessageFlags.EPHEMERAL
+                }
+            }
+
+            else if (args.type === "MESSAGE_UPDATE") {
+                const msg = MessageStore.getMessage(args.message.channel_id, args.message.id);
+                if (skipLog(msg)) return;
+
+                const newContent = args.message.content;
+                ctx.args[0].message.content = msg.content + " `[edited]`\n" + newContent;
+            }
+        });
+    }
+}
