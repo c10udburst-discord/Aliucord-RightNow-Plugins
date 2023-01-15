eafb6ab29c9b69945250ec78b86c0bd995d5fc61
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-01-15 13:02:34 +0100

    add emnity and vendetta compat

---
+++ EmnityCompat/index.ts
@@ -0,0 +1,17 @@
+import { Plugin } from "aliucord/entities";
+import { exists, download, readFile } from "aliucord/native/fs";
+import { ALIUCORD_DIRECTORY } from "aliucord/utils/constants";
+
+export default class EmnityCompat extends Plugin {
+    public async start() {
+        const downloadUrl = "https://raw.githubusercontent.com/vendetta-mod/builds/master/vendetta.js";
+        const filePath = ALIUCORD_DIRECTORY + "/vendetta.js"
+        
+        const run = () => eval(readFile(filePath));
+
+        if (!exists(filePath))
+            download(downloadUrl, filePath).then(run)
+        else
+            run();
+    }
+}
---
+++ VendettaCompat/index.ts
@@ -0,0 +1,17 @@
+import { Plugin } from "aliucord/entities";
+import { exists, download, readFile } from "aliucord/native/fs";
+import { ALIUCORD_DIRECTORY } from "aliucord/utils/constants";
+
+export default class VendettaCompat extends Plugin {
+    public async start() {
+        const downloadUrl = "https://raw.githubusercontent.com/enmity-mod/enmity/main/dist/Enmity.js";
+        const filePath = ALIUCORD_DIRECTORY + "/Enmity.js"
+        
+        const run = () => eval(readFile(filePath));
+
+        if (!exists(filePath))
+            download(downloadUrl, filePath).then(run)
+        else
+            run();
+    }
+}
