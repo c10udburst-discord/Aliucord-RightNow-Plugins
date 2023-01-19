76e7908b57369ae77d08b224c63c2cb1d134d809
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-01-19 08:33:52 +0100

    add plugin repo support

---
+++ buildAll.mjs
@@ -0,0 +1,41 @@
+import { spawnSync } from "child_process";
+import { platform } from "process";
+import { existsSync } from "fs";
+import { join } from "path";
+import { cwd } from "process";
+import { readdir } from "fs"
+
+readdir(cwd(), { withFileTypes: true }, (err, folders) => {
+    folders.forEach(folder => {
+        if(!folder.isDirectory()) return;
+        const folderName = folder.name;
+        const manifestPath = join(folderName, "manifest.json")
+        if(existsSync(manifestPath))
+        {
+            const path = join(folderName, "index.ts")
+            let path = null
+            if (existsSync(join(folderName, "index.ts"))) {
+                path = join(folderName, "index.ts")
+            } else if (existsSync(join(folderName, "index.tsx"))) {
+                path = join(folderName, "index.tsx")
+            }
+            if(!existsSync(path))
+            {
+                console.log(`Skipping ${folderName}`)
+                return;
+            }
+            const proc = spawnSync((platform === "win32") ? ".\\node_modules\\.bin\\rollup.cmd" : "node_modules/.bin/rollup", ["-c", "--configPlugin", "typescript", false].filter(Boolean), {
+                stdio: "inherit",
+                cwd: cwd(),
+                env: {
+                    plugin: folderName
+                }
+            });
+            
+            if (proc.error) {
+                console.error(proc.error)
+            }
+        }
+
+    })
+})
